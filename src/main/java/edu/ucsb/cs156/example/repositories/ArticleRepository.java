7afa576b4bc232f6b2dda475783b602b281e0788
Kate Larrick <katelarrick@umail.ucsb.edu>
2025-10-29 11:14:19 -0700

    Copied backend files from team01

---
+++ src/main/java/edu/ucsb/cs156/example/repositories/ArticleRepository.java
@@ -0,0 +1,9 @@
+package edu.ucsb.cs156.example.repositories;
+
+import edu.ucsb.cs156.example.entities.Article;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/** The ArticleRepository is a repository for Article entities. */
+@Repository
+public interface ArticleRepository extends CrudRepository<Article, Long> {}
